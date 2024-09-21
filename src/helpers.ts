d101c712c81b279c56025926c55002096f5c9c73
Yaroslav Pleshakov <yvnger@gmail.com>
2024-09-21 21:31:31 +0400

    Added support inputs

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,5 @@
+export function createEl(tag: 'input', ...classes: string[]): HTMLInputElement;
+export function createEl(tag: keyof HTMLElementTagNameMap, ...classes: string[]): HTMLElement;
 export function createEl(tag: string = 'div', ...classes: string[]): HTMLElement {
     const element = document.createElement(tag);
 
